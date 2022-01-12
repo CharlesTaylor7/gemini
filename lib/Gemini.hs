dfd7b5f432e0f3587a44fbb324e18f767655700a
Charles Taylor <charlestaylor95@gmail.com>
2022-01-11 22:53:44 -0500

    WIP

--- lib/Gemini.hs
+++ lib/Gemini.hs
@@ -21,7 +21,7 @@ import           Gemini.Types
 
 
 
--- | Store definition
+-- | Store operations
 data Store = Store
   { gemini  :: Gemini
   , history :: [Rotation]
@@ -31,6 +31,10 @@ data Store = Store
 
 instance Pretty Store where
   pretty Store { gemini } = show gemini
+
+
+applyRotation :: Rotation -> Store -> Store
+applyRotation r = over #gemini (rotate r) . over #history (r :)
 
 
 initialState :: Store
@@ -45,26 +49,47 @@ rootView :: MonadIO m => Store -> Html m Store
 rootView state =
   Html.div
     [ Html.className "gemini-app"
-    , Html.tabIndex 0
-    , Html.onKeydownM $ \key -> do
-      let rotation = case key of
-            -- the keyboard shortcuts are based on the top row of keys in the rightmost positions:
-            -- T, Y, U, I, O, P
-            Key.T -> Just $ Rotation LeftRing Clockwise
-            Key.Y -> Just $ Rotation LeftRing AntiClockwise
-            Key.U -> Just $ Rotation CenterRing Clockwise
-            Key.I -> Just $ Rotation CenterRing AntiClockwise
-            Key.O -> Just $ Rotation RightRing Clockwise
-            Key.P -> Just $ Rotation RightRing AntiClockwise
-            _     -> Nothing
+    , Html.onKeydown $ \key ->
+      case key of
+        -- the keyboard shortcuts are based on the top row of keys in the rightmost positions:
+        -- T, Y, U, I, O, P
+        Key.T -> applyRotation $ Rotation LeftRing Clockwise
+        Key.Y -> applyRotation $ Rotation LeftRing AntiClockwise
+        Key.U -> applyRotation $ Rotation CenterRing Clockwise
+        Key.I -> applyRotation $ Rotation CenterRing AntiClockwise
+        Key.O -> applyRotation $ Rotation RightRing Clockwise
+        Key.P -> applyRotation $ Rotation RightRing AntiClockwise
+        _     -> identity
 
-      pure $ case rotation of
-        Just r -> over #gemini (rotate r) . over #history (r :)
-        _      -> identity
     ]
-    [ geminiSvgView (state ^. #gemini)
+    [ buttonsRow
+    , geminiSvgView (state ^. #gemini)
     , debugView state
     ]
+
+
+buttonsRow :: Html m Store
+buttonsRow =
+  Html.div
+    [ Html.className "motion-buttons-row"
+    ]
+    ( flip map rotations $ \rotation ->
+        Html.button
+          [ Html.className "motion-button"
+          , Html.onClick $ applyRotation rotation
+          ]
+          [ Html.text $ prettyCompactText rotation
+          ]
+    )
+  where
+    rotations =
+      [ Rotation LeftRing Clockwise
+      , Rotation LeftRing AntiClockwise
+      , Rotation CenterRing Clockwise
+      , Rotation CenterRing AntiClockwise
+      , Rotation RightRing Clockwise
+      , Rotation RightRing AntiClockwise
+      ]
 
 debugView :: Store -> Html m a
 debugView state =
