66464680c8aa5a4321ed9216f80c7fc405b166ba
Cleo Taylor <cleotaylor@Cleos-MBP.home>
2022-01-18 16:01:08 -0500

    Deploy to fly

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ docker build --memory-swap -1 -t ninjapenguin/gemini-server .
 docker push ninjapenguin/gemini-server
 
 # Deploy to fly.io
-# can't deploy to fly, they won't pull containers that too large
-# our container is about 10gb
-# fly deploy --detach
+fly deploy --detach
 
 # Deploy to heroku 
 heroku container:push web
