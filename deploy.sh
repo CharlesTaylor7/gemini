a24c65ee54862da9470f45950f6b1ccdd435f883
Cleo Taylor <cleotaylor@Cleos-MBP.home>
2022-01-18 11:40:36 -0500

    Write deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 # On mac, the build of jsaddle-dom takes 90-120 minutes
 # The build runs out of memory unless I remove the swap limit 
-docker build --memory-swap -1 .
+docker build --memory-swap -1 -t ninjapenguin/gemini-server .
+docker push ninjapenguin/gemini-server
 
-# Use heroku's docker image registry 
+# Deploy to fly.io
+fly deploy --detach
+
+# Deploy to heroku 
 heroku container:push web
-
-# Deploy
 heroku container:release web
