ae86a0a27d0a0edfb8a0bda7cae20d1ee5421da4
Charles Taylor <charlestaylor95@gmail.com>
2022-01-19 16:34:16 -0500

    Organize scripts

---
+++ lib/Gemini/Env.hs
@@ -0,0 +1,37 @@
+module Gemini.Env 
+  ( EnvVarException(..)
+  , envOptional, envRequired
+  ) where
+
+import           Relude
+import           Control.Exception (throwIO)
+import           System.Environment          (lookupEnv)
+
+
+
+data EnvVarException = Unset String | InvalidFormat String
+  deriving stock (Eq, Show)
+  deriving anyclass (Exception)
+
+
+-- | Provide additional context to the failure case by upgrading a Maybe to an Either
+note :: e -> Maybe a -> Either e a
+note _ (Just x) = Right x
+note e _        = Left e
+
+
+envOptional :: Read a => String -> a -> IO a
+envOptional var def = do
+  maybeVar <- lookupEnv var
+  pure $ fromMaybe def $ do
+    var <- maybeVar
+    readMaybe var
+
+
+envRequired :: Read a => String -> IO a
+envRequired var = do
+  maybeVar <- lookupEnv var
+  let converted = do
+      var <- maybeVar & note (Unset var)
+      readMaybe var & note (InvalidFormat var)
+  converted & either throwIO pure
