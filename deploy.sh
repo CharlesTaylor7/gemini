b3b1f95a9a374d676d39c97f5107c3a94e71fe2a
Cleo Taylor <cleotaylor@Cleos-MBP.home>
2022-01-18 20:16:20 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,20 @@
+if test -n "$(git status --porcelain)"; then
+  echo "Can only deploy from a clean index; commit or remove uncommited changes"
+  exit 1
+fi
+
+IMAGE_NAME="ninjapenguin/gemini-server"
+COMMIT_HASH=$(git rev-parse HEAD)
+
+
 # On mac, the build of jsaddle-dom takes 90-120 minutes
 # The build runs out of memory unless I remove the swap limit 
-docker build --memory-swap -1 -t ninjapenguin/gemini-server .
-docker push ninjapenguin/gemini-server
+echo "1. Build docker image"
+docker build --memory-swap -1 -t $IMAGE_NAME  .
 
-# Deploy to fly.io
+echo "2. Push docker image"
+docker push $IMAGE_NAME:latest
+
+echo "3. Deploy to Fly.io"
 fly deploy
-
-# Deploy to heroku 
-# heroku container:push web
-# heroku container:release web
+fly open
