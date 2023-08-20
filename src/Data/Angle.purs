79cc87e46c832ac400fda1b2ad612e54b637c266
Charles Taylor <charlestaylor95@gmail.com>
2023-08-20 16:20:18 -0400

    Copy Haskell utility modules over as purescript

---
+++ src/Data/Angle.purs
@@ -0,0 +1,72 @@
+{-# language MultiParamTypeClasses #-}
+module Data.Angle
+  ( AngleUnit(..)
+  -- | Angle uses smart constructors and destructors to ensure units are handled appropriately
+  , Angle, pattern Radians, pattern Degrees, pattern Turns
+
+  -- trigonometic functions
+  , sine, cosine, arctan
+  )
+  where
+
+import           Relude
+
+import           Data.Group
+
+
+data AngleUnit
+  = Radian
+  | Degree
+  | Turn
+  -- ^ How many turns of a circle? e.g. 1 turn is equal to 2 pi radians and 360 degrees.
+
+
+-- | Opaque data type. Angles are internally stored as a 'Double' in radians.
+-- Construct or match on the amount of the angle by using one of these bi-directional pattern synonyms:
+-- `Radians`, `Degrees`, `Turns`
+newtype Angle = Angle Double
+  deriving stock (Eq, Ord, Show, Generic)
+  deriving anyclass (NFData)
+  deriving (Semigroup, Monoid, Group) via (Sum Double)
+
+
+-- | Construct or match on the radians of an angle
+pattern Radians :: Double -> Angle
+pattern Radians radians = Angle radians
+{-# COMPLETE Radians #-}
+
+-- | Construct or match on the degrees of an angle
+pattern Degrees :: Double -> Angle
+pattern Degrees degrees <- Angle (radiansToDegrees -> degrees) where
+  Degrees degrees = Radians $ degreesToRadians degrees
+{-# COMPLETE Degrees #-}
+
+radiansToDegrees :: Double -> Double
+radiansToDegrees radians = radians * 180 / pi
+
+degreesToRadians :: Double -> Double
+degreesToRadians degrees = degrees * pi / 180
+
+-- | Construct or match on the turns of an angle
+pattern Turns :: Double -> Angle
+pattern Turns turns <- Angle (radiansToTurns -> turns) where
+  Turns turns = Radians $ turnsToRadians turns
+{-# COMPLETE Turns #-}
+
+radiansToTurns :: Double -> Double
+radiansToTurns radians = radians / (2 * pi)
+
+turnsToRadians :: Double -> Double
+turnsToRadians turns   =  turns * 2 * pi
+
+
+sine :: Angle -> Double
+sine (Radians radians) = sin radians
+
+
+cosine :: Angle -> Double
+cosine (Radians radians) = cos radians
+
+
+arctan :: Double -> Double -> Angle
+arctan y x = Radians $ atan2 y x
---
+++ src/Data/Point.purs
@@ -0,0 +1,49 @@
+module Data.Point
+  ( Point(..)
+  , norm, angleToOrigin
+  )
+  where
+
+import           Relude
+
+import           Data.Aeson    (FromJSON, ToJSON)
+import           Data.Angle
+import           Data.Group
+import           Prettyprinter (Pretty (..))
+
+
+data Point = Point
+  { x :: !Double
+  , y :: !Double
+  }
+  deriving stock (Eq, Show, Generic)
+  deriving anyclass (FromJSON, ToJSON)
+  deriving anyclass (NFData)
+
+
+instance Pretty Point where
+  pretty (Point x y)
+    = "("
+    <> pretty x
+    <> ", "
+    <> pretty y
+    <> ")"
+
+instance Semigroup Point where
+  Point x1 y1 <> Point x2 y2 = Point (x1 + x2) (y1 + y2)
+
+instance Monoid Point where
+  mempty = Point 0 0
+
+instance Group Point where
+  invert (Point x y) = Point (-x) (-y)
+
+
+-- ## Operations
+-- | Distance to the origin
+norm :: Point -> Double
+norm Point { x, y } = sqrt $ x*x + y*y
+
+-- | Angle the point makes with the origin
+angleToOrigin :: Point -> Angle
+angleToOrigin (Point x y) = arctan y x
