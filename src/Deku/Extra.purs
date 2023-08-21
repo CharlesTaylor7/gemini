ea9f08923ba733421d18038e8cea7847ecc86c79
Charles Taylor <charlestaylor95@gmail.com>
2023-08-20 20:51:20 -0400

    WIP

--- src/Deku/Extra.purs
+++ src/Deku/Extra.purs
@@ -4,11 +4,13 @@ module Deku.Extra
   ) where
 
 import Prelude
-import Prim.Row (class Cons)
+import Prim.Row as Row
+import Type.RowList (class RowToList, class ListToRow, RowList)
 import Data.Symbol (class IsSymbol, reflectSymbol)
 import FRP.Event (Event)
-import Type.RowList
-import Type.Proxy
+import Type.RowList (Cons, Nil)
+import Type.Proxy (Proxy(..))
+import Unsafe.Coerce (unsafeCoerce)
 
 -- TODO: transform a Record<string, event> into a Event<string> class
 
@@ -16,16 +18,20 @@ class Class row where
   --className :: Record row -> Event String
   className :: Record row -> String
 
-instance (RowToList row list, ClassRowList list) => Class row where
+instance (RowToList row list, ListToRow list row, ClassRowList list) => Class row where
   className record = className_ record
 
 -- | Toy example that concatenates field names
 -- class Keys :: (r :: Row Type) -> Constraint
 class ClassRowList (list :: RowList Type) where
-  className_ :: forall row. RowToList row list => Record row -> String
+  className_ :: forall row. ListToRow list row => Record row -> String
 
 instance ClassRowList Nil where
-  className_ _ = ""
+  className_ _  = ""
 
 instance (ClassRowList tail, IsSymbol sym) => ClassRowList (Cons sym head tail) where
-  className_ _ = reflectSymbol (Proxy :: _ sym) <> " "<> className_ (Proxy :: _ tail)
+  className_ record = reflectSymbol (Proxy :: _ sym) <> " " <> className_ (narrow record)
+  -- (Proxy :: _ tail)
+
+narrow :: forall row tail. Row.Cons _ _ tail row => Record row -> Record tail
+narrow = unsafeCoerce
