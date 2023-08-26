f0eecbb05a8dd8ebcf178714ff4d7dc77631e793
Charles Taylor <charlestaylor95@gmail.com>
2023-08-26 19:15:12 -0400

    Refactor resize

--- src/Resize.purs
+++ src/Resize.purs
@@ -10,51 +10,39 @@ import Control.Monad.ST.Ref (STRef)
 import Control.Monad.ST.Ref as Ref
 import Effect (Effect)
 import Effect.Uncurried (EffectFn2)
+import Effect.Unsafe (unsafePerformEffect)
 import FRP.Event (Event, makeEvent)
 import Partial.Unsafe (unsafeCrashWith, unsafePartial)
 import Web.DOM (Element)
 
 observe :: { event :: Event DomRect, listen :: Element -> Effect Unit }
-observe = { event: event ob, listen: subscribe ob } 
-  where ob = observer
+observe = 
+  { event: makeEvent \pusher -> do
+      let { ref, ob } = observer
+      _ <- toEffect $ Ref.write pusher ref
+      pure $ ob # disconnectF
 
-observer :: Effect Observer
-observer = do
-  ref <- toEffect $ Ref.new mempty
-  ob <- unsafePartial (
-    newResizeObserverF $ \[{ target }] -> do
-      pusher <- toEffect $ Ref.read ref
-      pusher =<< getBoundingClientRectF target
-  )
-  pure $ Observer { ref, ob }
+  , listen: \el -> do 
+      let { ob } = observer
+      ob # disconnectF
+      ob # observeF el
+  } 
 
-subscribe :: Effect Observer -> Element -> Effect Unit
-subscribe ob el = do
-  Observer { ob } <- ob
-  ob # disconnectF
-  ob # observeF el
-
-event :: Effect Observer -> Event DomRect 
-event ob = makeEvent $ \pusher -> do
-  Observer { ref, ob } <- ob
-  _ <- toEffect $ Ref.write pusher ref
-  pure $ ob # disconnectF
-   
-
-onResize :: Element -> Event DomRect
-onResize el = makeEvent $ \pusher -> do
-
-  pusher =<< getBoundingClientRectF el
-
-  ob <- newResizeObserverF $ \_ -> 
-    pusher =<< getBoundingClientRectF el
-
-  ob # observeF el 
-
-  pure $ ob # disconnectF
+  where
+    observer :: Observer
+    observer = unsafePerformEffect $ do
+      ref <- toEffect $ Ref.new mempty
+      let 
+        ob = 
+          unsafePartial (
+            newResizeObserverF $ \[{ target }] -> do
+              pusher <- toEffect $ Ref.read ref
+              pusher =<< getBoundingClientRectF target
+          )
+      pure $ { ref, ob }
 
 
-newtype Observer = Observer
+type Observer = 
   { ob :: ForeignObserver
   , ref :: STRef Global (DomRect -> Effect Unit) 
   }
@@ -66,7 +54,7 @@ type Listener = Array { target :: Element } -> Effect Unit
 
 foreign import data DomRect :: Type
 foreign import getBoundingClientRectF :: Element -> Effect DomRect
-foreign import newResizeObserverF :: Listener -> Effect ForeignObserver
+foreign import newResizeObserverF :: Listener -> ForeignObserver
 foreign import observeF :: Element -> ForeignObserver -> Effect Unit
 foreign import disconnectF :: ForeignObserver -> Effect Unit
 -- foreign import unobserve :: Element -> Observer -> Effect Unit
