efb642343b81aabbceefde0042597d0576eb8069
Charles Taylor <charlestaylor95@gmail.com>
2023-08-26 14:26:25 -0400

    Event based API

--- src/Resize.purs
+++ src/Resize.purs
@@ -4,6 +4,18 @@ import Prelude
 import Effect (Effect)
 import Effect.Uncurried (EffectFn2)
 import Web.DOM (Element)
+import Partial.Unsafe (unsafeCrashWith)
+import FRP.Event (Event, makeEvent)
+
+foreign import data DomRect :: Type
+
+onResize :: Element -> Event DomRect
+onResize el = makeEvent ?callback
+  --unsafeCrashWith ""
+{- What I need is simply to attach an observer to a particular element
+-}
+
+{- full sketch of the API
 
 foreign import data Observer :: Type
 foreign import data Entry :: Type
@@ -13,3 +25,4 @@ foreign import newResizeObserver :: Callback -> Effect Observer
 foreign import observe :: Observer -> Element -> Effect Unit
 foreign import unobserve :: Observer -> Element -> Effect Unit
 foreign import disconnect :: Observer -> Effect Unit
+-}
