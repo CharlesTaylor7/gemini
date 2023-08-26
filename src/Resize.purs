46dddf592588b3dd42f0ac955ef0bed6f181a1e4
Charles Taylor <charlestaylor95@gmail.com>
2023-08-26 16:46:47 -0400

    Reimplement observer

--- src/Resize.purs
+++ src/Resize.purs
@@ -3,11 +3,15 @@ module Resize
   , Observer
   , onResize
   , observer
-  , subscribe
-  , event
+  -- , subscribe
+  -- , event
   ) where
 
 import Prelude
+import Control.Monad.ST 
+import Control.Monad.ST.Global
+import Control.Monad.ST.Ref (STRef)
+import Control.Monad.ST.Ref as Ref
 import Effect (Effect)
 import Effect.Uncurried (EffectFn2)
 import FRP.Event (Event, makeEvent)
@@ -15,7 +19,6 @@ import Partial.Unsafe (unsafeCrashWith, unsafePartial)
 import Web.DOM (Element)
 
 
-type Subscribe = (DomRect -> Effect Unit) -> Effect (Effect Unit)
 
 onResize :: Element -> Event DomRect
 onResize el = makeEvent $ \pusher -> do
@@ -30,12 +33,17 @@ onResize el = makeEvent $ \pusher -> do
   pure $ ob # disconnect
 
 
-observer :: (DomRect -> Effect Unit) -> Effect Observer
-observer pusher = unsafePartial (
-    newResizeObserver $ \[{ target }] -> 
+observer :: Effect Observer
+observer = do
+  ref <- toEffect $ Ref.new mempty
+  ob <- unsafePartial (
+    newResizeObserver $ \[{ target }] -> do
+      pusher <- toEffect $ Ref.read ref
       pusher =<< getBoundingClientRect target
   )
+  pure $ Observer { ref, ob }
 
+{-
 subscribe :: Element -> Effect Observer -> Effect Unit
 subscribe el ob = do
   ob <- ob
@@ -46,27 +54,21 @@ event :: Effect Observer -> Event DomRect
 event ob = makeEvent $ \_pusher -> do
   ob <- ob
   pure $ ob # disconnect
-
-
---makeEvent $ \pusher -> do
-
-  -- pusher =<< getBoundingClientRect el
-
-
-  -- ob # observe el 
-
-  -- pure $ ob # disconnect
+-}
    
-   
+newtype Observer = Observer
+  { ob :: ForeignObserver
+  , ref :: STRef Global (DomRect -> Effect Unit) 
+  }
 
-foreign import data Observer :: Type
+foreign import data ForeignObserver :: Type
 
 
 type Listener = Array { target :: Element } -> Effect Unit
 
 foreign import data DomRect :: Type
 foreign import getBoundingClientRect :: Element -> Effect DomRect
-foreign import newResizeObserver :: Listener -> Effect Observer
-foreign import observe :: Element -> Observer -> Effect Unit
-foreign import disconnect :: Observer -> Effect Unit
+foreign import newResizeObserver :: Listener -> Effect ForeignObserver
+foreign import observe :: Element -> ForeignObserver -> Effect Unit
+foreign import disconnect :: ForeignObserver -> Effect Unit
 -- foreign import unobserve :: Element -> Observer -> Effect Unit
