ff3422149b1fde6942a9c4afef6aa5f4c72662b6
Charles Taylor <charlestaylor95@gmail.com>
2023-09-14 15:16:59 -0400

    cyclic spec

--- test/Test.purs
+++ test/Test.purs
@@ -6,21 +6,60 @@ import Effect.Class.Console (log)
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
 import Effect.Aff (launchAff_, delay)
+import Data.Cyclic
+import Data.Nat (class Pos, D2, D18, D54)
 import Test.Spec (Spec, pending, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = launchAff_ $ runSpec [ consoleReporter ] spec
+main = launchAff_ $ runSpec [ consoleReporter ] $ do
+  dataCyclicSpec
 
-spec :: Spec Unit
-spec =
-  describe "Attributes" do
-    it "runs in NodeJS" $ pure unit
-    it "runs in the browser" $ pure unit
-    it "supports streaming reporters" $ pure unit
-    it "supports async specs" do
-      res <- delay (Milliseconds 100.0) $> "Alligator"
-      res `shouldEqual` "Alligator"
-    it "is PureScript 0.12.x compatible" $ pure unit
+
+dataCyclicSpec :: Spec Unit
+dataCyclicSpec = do
+  describe "Data.Cyclic" $ do
+    describe "methods" $ do
+      it "creation & destruction" $ do
+        let n = unCyclic $ (cyclic 3 :: Cyclic D2)
+        n `shouldEqual` 1
+{-
+      it "compareCyclic" $ do
+        ((0 :: Cyclic D6) `compareCyclic` 3) `shouldEqual` Opposite
+        ((0 :: Cyclic D6) `compareCyclic` 6) `shouldEqual` Equal
+        ((1 :: Cyclic D7) `compareCyclic` 4) `shouldEqual` Precedes
+        ((1 :: Cyclic D5) `compareCyclic` 4) `shouldEqual` Exceeds
+
+    describe "Num instance" $ do
+      it "integer literals" $ do
+        unCyclic D@10 3 `shouldEqual` 3
+        unCyclic D@10 11 `shouldEqual` 1
+
+      it "addition" $ do
+        unCyclic D@10 (4 + 9) `shouldEqual` 3
+
+      it "subtraction" $ do
+        unCyclic D@10 (4 - 9) `shouldEqual` 5
+
+      it "multiplication" $ do
+        unCyclic D@10 (3 * 7) `shouldEqual` 1
+
+      it "signum" $ do
+        -- | always 1
+        unCyclic D(signum (-4) :: Cyclic D10) `shouldEqual` 1
+
+    describe "Group instance" $ do
+      it "addition should be the same as the group mappend" $
+        2 <> 3 `shouldEqual` (2 <> 3 :: Cyclic D17)
+
+      it "subtraction should be the same as the group subtraction" $
+        2 - 3 `shouldEqual` (2 ~~ 3 :: Cyclic D17)
+
+      it "can fold an empty list" $ do
+        unCyclic D@3 (fold []) `shouldEqual` 0
+
+      it "can fold a nonempty list" $ do
+        unCyclic D@10 (fold [3, 5, 7]) `shouldEqual` 5
+        -}
