9b351573b77183f0a28f5da9fc07af5c2de0d4a8
Charles Taylor <charlestaylor95@gmail.com>
2023-09-24 13:45:56 -0400

    Cleanup main

--- src/Main.purs
+++ src/Main.purs
@@ -1,25 +1,10 @@
 module Main where
 
-import Data.Foldable
-import Data.Location
-import Data.Permutation
-import Debug
 import Prelude
-import Test.Gemini.Gen
 
-import Deku.Toplevel (runInBody, runInBody')
+import Deku.Toplevel (runInBody)
 import Effect (Effect)
-import Effect.Console as Console
 import Gemini.Component.App as App
-import Test.QuickCheck.Gen as Gen
-
-mainDev :: Effect (Effect Unit)
-mainDev = runInBody' App.component
 
 main :: Effect Unit
-main = do
-  sample <- Gen.randomSample $
-    arbitrary <#> \(AnyPermutation p) -> (p :: _ 2)
-  Console.log $ show sample
-
-  runInBody App.component
+main = runInBody App.component
